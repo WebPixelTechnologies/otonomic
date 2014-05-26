bde8d4907e4f3840d0ad233309213831093f7947
root <root@ip-10-107-2-199.eu-west-1.compute.internal>
2014-05-26 14:01:28 +0000

    searching wp works

--- js/otonomic/search_filterv1.0.2-wp.js
+++ js/otonomic/search_filterv1.0.2-wp.js
@@ -12,6 +12,45 @@ var already_searched = false;
 
 var redirect_url = '';
 
+function parseURL(url){
+    parsed_url = {}
+
+    if ( url == null || url.length == 0 )
+        return parsed_url;
+
+    protocol_i = url.indexOf('://');
+    parsed_url.protocol = url.substr(0,protocol_i);
+
+    remaining_url = url.substr(protocol_i + 3, url.length);
+    domain_i = remaining_url.indexOf('/');
+    domain_i = domain_i == -1 ? remaining_url.length - 1 : domain_i;
+    parsed_url.domain = remaining_url.substr(0, domain_i);
+    parsed_url.path = domain_i == -1 || domain_i + 1 == remaining_url.length ? null : remaining_url.substr(domain_i + 1, remaining_url.length);
+
+    domain_parts = parsed_url.domain.split('.');
+    switch ( domain_parts.length ){
+        case 2:
+          parsed_url.subdomain = null;
+          parsed_url.host = domain_parts[0];
+          parsed_url.tld = domain_parts[1];
+          break;
+        case 3:
+          parsed_url.subdomain = domain_parts[0];
+          parsed_url.host = domain_parts[1];
+          parsed_url.tld = domain_parts[2];
+          break;
+        case 4:
+          parsed_url.subdomain = domain_parts[0];
+          parsed_url.host = domain_parts[1];
+          parsed_url.tld = domain_parts[2] + '.' + domain_parts[3];
+          break;
+    }
+
+    parsed_url.parent_domain = parsed_url.host + '.' + parsed_url.tld;
+
+    return parsed_url;
+}
+
 function sleep(millis, callback) {
     setTimeout(function()
             { callback(); }
@@ -19,7 +58,11 @@ function sleep(millis, callback) {
 }
 
 function get_site_id(redirect_url){
-    $.get( redirect_url + "/sites/view/.json", function( data ) {
+    
+site_json_url = parseURL(redirect_url).domain;
+console.log("site json: "+ site_json_url);
+
+$.get( "http://"+ site_json_url + "/sites/view/.json", function( data ) {
         
         sleep(5000, function(){
              create_wp_site(data.id, data.slug);
@@ -72,7 +115,9 @@ function create_wp_site(site_id, slug){
     }
     $("#search_wrapper_main,#search_wrapper_floater").delegate(".search-results-item", 'click', function (e) {
         e.preventDefault();
-        $.ajax({
+        $('#btn_go').text('loading...');
+
+	$.ajax({
           type: "get",
           url: $(this).attr('href')
         })
@@ -80,12 +125,17 @@ function create_wp_site(site_id, slug){
             responseObj = $.parseJSON(response);
             if (responseObj.status === 'success') {
                 redirect_url =responseObj.redirect;
-            
-                sleep(7000, function(){
+		
+
+		console.log("response: "+ responseObj.redirect);        
+    
+               // showLoader('Your site is being created for you!', true);
+		sleep(7000, function(){
                     get_site_id(redirect_url);
                 });
+		//showLoader('Your site is being created for you!', true);               
                 
-                return false;
+		return false;
             };
         });
         var page_name = $('p.media-heading', this).html();
@@ -365,7 +415,8 @@ function counterLoader(counterElementId){
                         $('.loading-content .loading-counter-text').html('<p>Your website is ready</p><a href="'+redirect_url+'" class="btn btn-lg btn-ttc" class="btn_go btn btn-ttc track_event" data-ga-category="Marketing Website"  data-ga-event="site created ie9fix" data-ajax-track="1">See Your Website</a>');
                     }
                     // All other browsers
-                    else { window.location.href = redirect_url; }
+                    else {
+			 window.location.href = redirect_url; }
                     },1000);
 
                     clearInterval(interval);
