6c91b8c97f42dad22f936bcfd182b05dbda11747
franksoftgrid <frank@softgrid.in>
2014-08-11 14:58:52 +0530

    Completed jobs page design
    Completed jobs page design

--- pages/jobs/index.php
+++ pages/jobs/index.php
@@ -1,130 +1,109 @@
 <!DOCTYPE html>
-<html class="no-js" xmlns="http://www.w3.org/1999/html">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<html>
+  <head>
     <title>Otonomic Jobs - Head of Online Media</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link rel="stylesheet" href="http://blog.otonomic.com/wp-content/plugins/otonomic-facebook-connect/css/tb.bootstrap.css">
-    <link rel="stylesheet" href="css/style.css">
-    <script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js'></script>
-    <script type='text/javascript' src='/js/otonomic-analytics.js'></script>
-
-</head>
-
-<body>
-<div class="tb">
-<header class="banner container" role="banner">
-    <div class="row">
-        <div class="col-lg-12">
-            <a class="brand" href="/">Otonomic</a>
-            <nav class="nav-main" role="navigation">
-            </nav>
-        </div>
-    </div>
-</header>
-
-<div class="row" id="main-content">
-    <div class="col-sm-12 col-xs-12">
-        <header>
-            <h1 class="entry-title">
-                Head of Online Media
-            </h1>
-        </header>
-
-        <div class="entry-content">
-            <div class="thumbnail">
-                <div class="caption">
-                    <p>
-                        Otonomic is the world's simplest website builder. In a single click, Otonomic turns a user's Facebook business page into a professional website that updates automatically with new content posted on Facebook and other social media. The company is backed by leading VCs and angel investors, and launched its product on May 2014 with over 30,000 sites created in the platform so far.
-                        </p>
-                    <p>
-                        We’re looking for a dedicated, motivated, creative, data-driven and resourceful head of online marketing to build the global brand and its global online marketing strategy. The selected candidate will be responsible for initiating, coordinating and implementing the company’s marketing activities, including brand management and online marketing (SEM, SEO, ads, etc.).
-                        </p>
-                    <p>
-                        Responsibilities
-                        <ul>
-                            <li>Strategic Planning - Take part in shaping and building an effective online marketing plan that will reach freelancers and small business owners, and help them grow their business.
-                            </li>
-                            <li>Brand management: Develop and establish Otonomic as a leading brand in website building through messaging, creative, and content promotion.
-                            </li>
-                            <li>Online Marketing - Implementing effective short and long term strategies to achieve commercial targets and goals. Leading all aspects of the paid search campaigns. (Budget, research, negotiation, control, analysis etc.)
-                            </li>
-                            <li>Market Research – Leading competitive analysis, customer segmentation, market penetration and product positioning.
-                            </li>
-                            <li>Marketing Communication – Branding, public relations, advertising, white papers & trade shows.
-                            </li>
-                            <li>Marketing Budget and control – Develop and manage the marketing budget whilst tracking KPI’s and ROI for all marketing activities.
-                            </li>
-                            <li>Build, manage and motivate the company’s marketing team.
-                            </li>
-                            <li>Reporting directly to CEO.
-                            </li>
-                        </ul>
-                    </p>
-                    <p>
-                        Requirements
-                        <ul>
-                            <li>4+ years of hands-on experience in online marketing.
-                            </li>
-                            <li>English at mother tongue level. Working Hebrew.
-                            </li>
-                            <li>Deep familiarity with the American market.
-                            </li>
-                            <li>Entrepreneurial spirit with motivation and passion to build and lead a global brand.
-                            </li>
-                            <li>Managerial experience – a big plus.
-                            </li>
-                        </ul>
-                    </p>
-                    <p>
-                        Please email your CVs to jobs@otonomic.com.
-                    </p>
-                </div>
-            </div>
-        </div>
-
-        <div id="tablet-dude"></div>
-    </div>
-</div>
-
-
-<script>
-    $('#shareme').sharrre({
-        share: {
-            googlePlus: true,
-            facebook: true,
-            twitter: true,
-            digg: false,
-            delicious: false,
-            stumbleupon: false,
-            linkedin: true,
-            pinterest: false
-        },
-        buttons: {
-            googlePlus: {size: 'tall', annotation:'bubble'},
-            facebook: {layout: 'box_count'},
-            twitter: {count: 'vertical'},
-            digg: {type: 'DiggMedium'},
-            delicious: {size: 'tall'},
-            stumbleupon: {layout: '5'},
-            linkedin: {counter: 'top'},
-            pinterest: {media: 'http://sharrre.com/img/example1.png', description: $('#shareme').data('text'), layout: 'vertical'}
-        },
-        enableHover: false,
-        enableCounter: false,
-        enableTracking: true
-    });
-</script>
-
-
-
-<footer class="content-info" role="contentinfo">
-    <div class="container">
-        <p>&copy; <?php echo date('Y'); ?> Otonomic</p>
-    </div>
-</footer>
-</div>
-</body>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="assets/css/fonts.css" rel="stylesheet" media="screen">
+    <!-- Bootstrap -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
+	<link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+	
+	<link href="assets/css/style.css" rel="stylesheet" media="screen">
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+	<script type='text/javascript' src='/js/otonomic-analytics.js'></script>
+  </head>
+  <body>
+  	<div id="header">
+		<div class="container">
+			<div class="pull-left">
+				<div id="logo">
+					<img src="assets/img/logo.png" />
+				</div>
+			</div>
+			<div class="pull-right">
+				Like
+			</div>
+		</div>
+	</div><!-- #header-ends -->
+	<div id="page-heading">
+		<div class="container">
+			<div class="row-fluid">
+				<div class="span8">
+					<h1 class="page-title">Head of online marketing</h1>
+				</div>
+				<div class="span4">
+					<img src="assets/img/team.png" class="heading-img" />
+				</div>
+			</div>
+		</div>
+	</div><!-- #page-heading ends -->
+	<div id="content">
+		<div class="container">
+			<div class="row-fluid">
+				<div class="span8">
+					<p>
+						Otonomic is the world's simplest website builder. In a single click, Otonomic turns a user's Facebook business page into a professional website that updates automatically with new content posted on Facebook and other social media. The company is backed by leading VCs and angel investors, and launched its product on May 2014 with over 30,000 sites created in the platform so far.
+					</p>
+					<p>
+						We're looking for a dedicated, motivated, creative, data-driven and resourceful head of online marketing to build the global brand and its global online marketing strategy. The selected candidate will be responsible for initiating, coordinating and implementing the company's marketing activities, including brand management and online marketing (SEM, SEO, ads, etc.).
+					</p>
+					<h2>Responsibilities</h2>
+					<p>
+						<strong>Strategic Planning</strong>: Take part in shaping and building an effective online marketing plan that will reach freelancers and small business owners, and help them grow their business.
+					</p>
+					<p>
+						<strong>Brand management</strong>: Develop and establish Otonomic as a leading brand in website building through messaging, creative, and content promotion.
+					</p>
+					<p>
+						<strong>Online Marketing</strong>: Implementing effective short and long term strategies to achieve commercial targets and goals. Leading all aspects of the paid search campaigns. (Budget, research, negotiation, control, analysis etc.)
+					</p>
+					<p>
+						<strong>Market Research</strong>: Leading competitive analysis, customer segmentation, market penetration and product positioning.
+					</p>
+					<p>
+						<strong>Marketing Communication</strong>: Branding, public relations, advertising, white papers & trade shows.
+					</p>
+					<p>
+						<strong>Marketing Budget and control</strong>: Develop and manage the marketing budget whilst tracking KPI's and ROI for all marketing activities.
+					</p>
+					<p>
+						<strong>Build, manage and motivate</strong> the company's marketing team
+					</p>
+					<p>
+						<strong>Reporting directly to CEO</strong>
+					</p>
+					<h2>Requirements</h2>
+					<p>
+						<strong>4+ years</strong> of hands-on experience in online marketing
+					</p>
+					<p>
+						<strong>English</strong> at mother tongue level. Working Hebrew
+					</p>
+					<p>
+						<strong>Deep familiarity with the American market</strong>
+					</p>
+					<p>
+						<strong>Entrepreneurial spirit</strong> with motivation and passion to build and lead a global
+					</p>
+					<p>
+						<strong>Managerial experience</strong> - a big plus
+					</p>
+				</div>
+				<div class="span4">
+					
+				</div>
+			</div>
+		</div>
+	</div><!-- #content ends -->
+	<div id="footer">
+		<div class="row-fluid">
+			<div class="span7 offset5">
+				<div class="pull-right align-right">
+					<img src="assets/img/tablet-dude.png" id="footer-overlap" />
+				</div>
+			</div>
+		</div>
+	</div><!-- #footer ends -->
+  </body>
 </html>
