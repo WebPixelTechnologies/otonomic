c665757be674db0c5fc9e50f3669e666c98e6fb5
Omri Allouche <omri@otonomic.com>
2014-08-14 15:43:41 +0300

    Added job vp_rd.php

---
+++ pages/jobs/community_manager.php
@@ -0,0 +1,158 @@
+<?php $position = 'Community Manager'; ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Otonomic Jobs - Community Manager</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<meta name="description" content="Otonomic is looking for a new <?= $position?>. Please help by sharing!" />
+	<!-- Open Graph data --> 
+	<meta property="og:title" content="Otonomic Jobs - <?= $position?>" />
+	<meta property="og:type" content="article" /> 
+	<meta property="og:image" content="assets/img/jobs-banner.jpg" />
+	<meta property="og:description" content="Otonomic is looking for a new <?= $position?>. Please help by sharing!" />
+
+	
+	<link href="assets/css/fonts.css" rel="stylesheet" media="screen">
+    <!-- Bootstrap -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
+	<link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+	
+	<link href="assets/css/style.css" rel="stylesheet" media="screen">
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+	<script type='text/javascript' src='/js/otonomic-analytics.js'></script>
+	<script src="//platform.linkedin.com/in.js" type="text/javascript">
+	  lang: en_US
+	</script>
+  </head>
+  <body>
+  	<div id="header">
+		<div class="container">
+			<div class="pull-left">
+				<div id="logo">
+					<img src="assets/img/logo.png" />
+				</div>
+			</div>
+			<div class="pull-right">
+				<div id="page-like">
+					<div class="fb-like" data-href="http://www.facebook.com/otonomic/" data-layout="button_count" data-action="like" data-show-faces="true" data-share="false"></div>
+				</div>
+			</div>
+		</div>
+	</div><!-- #header-ends -->
+	<div id="page-heading">
+		<div class="container">
+			<div class="row-fluid">
+				<div class="span8">
+					<h1 class="page-title"><?= $position?></h1>
+				</div>
+				<div class="span4">
+					<img src="assets/img/team.png" class="heading-img" />
+				</div>
+			</div>
+		</div>
+	</div><!-- #page-heading ends -->
+	<div id="content">
+		<div class="container">
+			<div class="row-fluid">
+				<div class="span8">
+					<p>
+						Otonomic is the world's simplest website builder. In a single click, Otonomic turns a user's Facebook business page into a professional website that updates automatically with new content posted on Facebook and other social media. The company is backed by leading VCs and angel investors, and launched its product on May 2014 with over 30,000 sites created in the platform so far.
+					</p>
+					<p>
+                        We are looking for an experienced Community Manager to create, cultivate, and drive online communities for small business owners worldwide. You will be responsible for creating content, engaging with users and influencers, and managing communities on Facebook, Twitter and LinkedIn. You will be reporting to the VP Marketing.
+                    </p>
+                    <p>Please email your CV to <a href="mailto:jobs@otonomic.com">jobs@otonomic.com</a>.</p>
+
+					<h2>Responsibilities</h2>
+					<p>
+                        Manage the company's social media activities. This includes creating content, monitoring and engaging users on  the company's blog and social media channels.
+                    </p>
+                    <p>
+                        Build relationships with the community by engaging with small business owners and influencers on blogs, social media, webinars etc.
+                    </p>
+                    <p>
+                        Define key metrics on community engagement, analyze data and recommend improvements.
+                    </p>
+                    <p>
+                        Monitor on-going discussions and trends within the community. Identify and report trends and user needs and advises on potential opportunities.
+                    </p>
+                    <p>
+                        Provide  customer service.
+                    </p>
+
+					<h2>Requirements</h2>
+					<p>
+                        Mother tongue English. Familiarity with American culture a plus.
+					</p>
+                    <p>
+                        Excellent communications skills, especially written communications.
+                    </p>
+                    <p>
+                        At least 2 years experience in experience managing social media accounts or communities for an international brand.
+                    </p>
+                    <p>
+                        Familiarity with social media listening and management tools.
+                    </p>
+
+                    <p>Please email your CV to <a href="mailto:jobs@otonomic.com">jobs@otonomic.com</a>.</p>
+				</div>
+
+
+
+
+				<div class="span4">
+					<div id="share">
+						<ul class="social-share">
+							<li class="google-plus">
+								<div class="g-plus" data-action="share" data-annotation="vertical-bubble" data-height="60"></div>
+							</li>
+							<li class="facebook">
+								<!-- div class="fb-share-button" data-href="http://www.facebook.com/otonomic/"></div -->
+								<a name="fb_share" type="box_count">Share</a>
+							</li>
+							<li class="tweeter">
+								<a href="https://twitter.com/share" class="twitter-share-button" data-count="vertical">Tweet</a>
+							</li>
+							<li class="linkedin">
+								<script type="IN/Share" data-counter="top"></script>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div><!-- #content ends -->
+	<div id="footer">
+		<div class="row-fluid">
+			<div class="span7 offset5">
+				<div class="pull-right align-right">
+					<img src="assets/img/tablet-dude.png" id="footer-overlap" />
+				</div>
+			</div>
+		</div>
+	</div><!-- #footer ends -->
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=373931652687761&version=v2.0";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+	<script type="text/javascript">
+		(function() {
+		var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+		po.src = 'https://apis.google.com/js/platform.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+		})();
+	</script>
+	<script>
+		!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+	</script>
+	<script src="http://static.ak.fbcdn.net/connect.php/js/FB.Share" type="text/javascript"></script>
+  </body>
+</html>
---
+++ pages/jobs/vp_rd.php
@@ -0,0 +1,175 @@
+<?php $position = 'VP R&D'; ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Otonomic Jobs - <?= $position?></title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<meta name="description" content="Otonomic is looking for a new <?= $position?>. Please help by sharing!" />
+	<!-- Open Graph data --> 
+	<meta property="og:title" content="Otonomic Jobs - <?= $position?>" />
+	<meta property="og:type" content="article" /> 
+	<meta property="og:image" content="assets/img/jobs-banner.jpg" />
+	<meta property="og:description" content="Otonomic is looking for a new <?= $position?>. Please help by sharing!" />
+
+	
+	<link href="assets/css/fonts.css" rel="stylesheet" media="screen">
+    <!-- Bootstrap -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
+	<link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+	
+	<link href="assets/css/style.css" rel="stylesheet" media="screen">
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+	<script type='text/javascript' src='/js/otonomic-analytics.js'></script>
+	<script src="//platform.linkedin.com/in.js" type="text/javascript">
+	  lang: en_US
+	</script>
+  </head>
+  <body>
+  	<div id="header">
+		<div class="container">
+			<div class="pull-left">
+				<div id="logo">
+					<img src="assets/img/logo.png" />
+				</div>
+			</div>
+
+			<div class="pull-right">
+                <div id="page-like">
+					<div class="fb-like" data-href="http://www.facebook.com/otonomic/" data-layout="button_count" data-action="like" data-show-faces="true" data-share="false"></div>
+				</div>
+
+                <div id="nav-menu">
+                    <ul>
+                        <li>
+                            <a href="/pages/jobs/">Other Jobs</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+		</div>
+	</div><!-- #header-ends -->
+	<div id="page-heading">
+		<div class="container">
+			<div class="row-fluid">
+				<div class="span8">
+					<h1 class="page-title"><?= $position?></h1>
+				</div>
+				<div class="span4">
+					<img src="assets/img/team.png" class="heading-img" />
+				</div>
+			</div>
+		</div>
+	</div><!-- #page-heading ends -->
+	<div id="content">
+		<div class="container">
+			<div class="row-fluid">
+				<div class="span8">
+					<p>
+                        Otonomic is the world's simplest website builder. In a single click, Otonomic turns a user's Facebook business page into a professional website that updates automatically with new content posted on Facebook and other social media. The company is backed by leading VCs and angel investors, and launched its product on May 2014 with over 30,000 sites created using the platform so far.
+					</p>
+					<p>
+                        We're looking for a talented and passionate VP R&D to join our team. We work hard to make site creation easy, and use cutting edge technologies in a very creative manner to change the way the internet is used by small businesses. If you like to take existing web technologies one step further, email your CV to <a href="mailto:jobs@otonomic.com">jobs@otonomic.com</a> and let us buy you a cup of coffee!
+					</p>
+
+					<h2>Responsibilities</h2>
+					<p>
+                        Collaborate with Product, UX and BizDev in order to select the right technological solution to meet both strategic and tactical goals.
+                    </p>
+                    <p>
+                        Prioritize tasks, set deadlines, and direct the work of our brilliant development team members to meet set deadlines.
+                    </p>
+                    <p>
+                        Write beautiful code, mostly in PHP, and create a team culture for efficient, maintainable code.
+                    </p>
+                    <p>
+                        Report to the CEO.
+                    </p>
+
+					<h2>Requirements</h2>
+					<p>
+                        4+ year experience developing web applications in PHP.
+                    </p>
+                    <p>
+                        Strong attention to details sprayed over a problem-solving thinking ability.
+                    </p>
+                    <p>
+                        Fluency in HTML, CSS, JavaScript and jQuery.
+                    </p>
+                    <p>
+                        True love for thy users.
+                    </p>
+
+                    <h2>Pluses</h2>
+                    <p>
+                        Management experience leading other developers.
+                    </p>
+                    <p>
+                        Bachelor's Degree in Computer Science.
+                    </p>
+                    <p>
+                        Experience with Wordpress/Drupal, Facebook API, client side frameworks (e.g. Angular.JS/Ember/Backbone), AWS, Sys admin, TDD.
+                    </p>
+
+                    <p>Interested in applying? Sweet! Send your CV and any technical side projects, open source contributions, and personal websites/blogs to <a href="mailto:jobs@otonomic.com">jobs@otonomic.com</a>.</p>
+				</div>
+
+
+
+
+				<div class="span4">
+					<div id="share">
+						<ul class="social-share">
+							<li class="google-plus">
+								<div class="g-plus" data-action="share" data-annotation="vertical-bubble" data-height="60"></div>
+							</li>
+							<li class="facebook">
+								<!-- div class="fb-share-button" data-href="http://www.facebook.com/otonomic/"></div -->
+								<a name="fb_share" type="box_count">Share</a>
+							</li>
+							<li class="tweeter">
+								<a href="https://twitter.com/share" class="twitter-share-button" data-count="vertical">Tweet</a>
+							</li>
+							<li class="linkedin">
+								<script type="IN/Share" data-counter="top"></script>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div><!-- #content ends -->
+	<div id="footer">
+		<div class="row-fluid">
+			<div class="span7 offset5">
+				<div class="pull-right align-right">
+					<img src="assets/img/tablet-dude.png" id="footer-overlap" />
+				</div>
+			</div>
+		</div>
+	</div><!-- #footer ends -->
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=373931652687761&version=v2.0";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+	<script type="text/javascript">
+		(function() {
+		var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+		po.src = 'https://apis.google.com/js/platform.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+		})();
+	</script>
+	<script>
+		!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+	</script>
+	<script src="http://static.ak.fbcdn.net/connect.php/js/FB.Share" type="text/javascript"></script>
+  </body>
+</html>
