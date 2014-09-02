af7c99577cf6450c345fb4924dae0d4cafb27470
Omri Allouche <omri@otonomic.com>
2014-09-02 23:52:42 +0300

    Added link to head of online marketing position

---
+++ pages/jobs/head_of_online_marketing.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Otonomic Jobs - Head of Online Marketing</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<meta name="description" content="Otonomic is looking for a new Head of Online Marketing. Please help by sharing!" />
+	<!-- Open Graph data --> 
+	<meta property="og:title" content="Otonomic Jobs - Head of Online Marketing" />
+	<meta property="og:type" content="article" />
+    <meta property="og:url" content="http://otonomic.com/pages/jobs/" />
+	<meta property="og:image" content="http://otonomic.com/pages/jobs/assets/img/Jobs-Banner-head-of-online-marketing.jpg" />
+	<meta property="og:description" content="Otonomic is looking for a new Head of Online Marketing. Please help by sharing!" />
+    <meta property="fb:admins" content="19717048">
+    <meta property="fb:app_id" content="334469486646650">
+
+	
+	<link href="assets/css/fonts.css" rel="stylesheet" media="screen">
+    <!-- Bootstrap -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
+	<link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+	
+	<link href="assets/css/style.css" rel="stylesheet" media="screen">
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+	<script type='text/javascript' src='/js/otonomic-analytics.js'></script>
+	<script src="//platform.linkedin.com/in.js" type="text/javascript">
+	  lang: en_US
+	</script>
+  </head>
+  <body>
+  	<div id="header">
+		<div class="container">
+			<div class="pull-left">
+				<div id="logo">
+					<img src="assets/img/logo.png" />
+				</div>
+			</div>
+			<div class="pull-right">
+				<div id="page-like">
+					<div class="fb-like" data-href="http://www.facebook.com/otonomic/" data-layout="button_count" data-action="like" data-show-faces="true" data-share="false"></div>
+				</div>
+			</div>
+		</div>
+	</div><!-- #header-ends -->
+	<div id="page-heading">
+		<div class="container">
+			<div class="row-fluid">
+				<div class="span8">
+					<h1 class="page-title">Head of online marketing</h1>
+				</div>
+				<div class="span4">
+					<img src="assets/img/team.png" class="heading-img" />
+				</div>
+			</div>
+		</div>
+	</div><!-- #page-heading ends -->
+	<div id="content">
+		<div class="container">
+			<div class="row-fluid">
+				<div class="span8">
+					<p>
+						Otonomic is the world's simplest website builder. In a single click, Otonomic turns a user's Facebook business page into a professional website that updates automatically with new content posted on Facebook and other social media. The company is backed by leading VCs and angel investors, and launched its product on May 2014 with over 30,000 sites created in the platform so far.
+					</p>
+					<p>
+						We're looking for a dedicated, motivated, creative, data-driven and resourceful head of online marketing to build the global brand and its global online marketing strategy. The selected candidate will be responsible for initiating, coordinating and implementing the company's marketing activities, including brand management and online marketing (SEM, SEO, ads, etc.).
+					</p>
+                    <p>Please email your CV to <a href="mailto:jobs@otonomic.com">jobs@otonomic.com</a>.</p>
+
+					<h2>Responsibilities</h2>
+					<p>
+						<strong>Strategic Planning</strong>: Take part in shaping and building an effective online marketing plan that will reach freelancers and small business owners, and help them grow their business.
+					</p>
+					<p>
+						<strong>Brand management</strong>: Develop and establish Otonomic as a leading brand in website building through messaging, creative, and content promotion.
+					</p>
+					<p>
+						<strong>Online Marketing</strong>: Implementing effective short and long term strategies to achieve commercial targets and goals. Leading all aspects of the paid search campaigns. (Budget, research, negotiation, control, analysis etc.)
+					</p>
+					<p>
+						<strong>Market Research</strong>: Leading competitive analysis, customer segmentation, market penetration and product positioning.
+					</p>
+					<p>
+						<strong>Marketing Communication</strong>: Branding, public relations, advertising, white papers & trade shows.
+					</p>
+					<p>
+						<strong>Marketing Budget and control</strong>: Develop and manage the marketing budget whilst tracking KPI's and ROI for all marketing activities.
+					</p>
+					<p>
+						<strong>Build, manage and motivate</strong> the company's marketing team.
+					</p>
+					<p>
+						<strong>Reporting directly to CEO</strong>
+					</p>
+					<h2>Requirements</h2>
+					<p>
+						<strong>4+ years</strong> of hands-on experience in online marketing.
+					</p>
+					<p>
+						<strong>English</strong> at mother tongue level. Working Hebrew.
+					</p>
+					<p>
+						<strong>Deep familiarity with the American market</strong>.
+					</p>
+					<p>
+						<strong>Entrepreneurial spirit</strong> with motivation and passion to build and lead a global brand.
+					</p>
+					<p>
+						<strong>Managerial experience</strong> - a big plus.
+					</p>
+                    <p>Please email your CV to <a href="mailto:jobs@otonomic.com">jobs@otonomic.com</a>.</p>
+				</div>
+				<div class="span4">
+					<div id="share">
+						<ul class="social-share">
+							<li class="google-plus">
+								<div class="g-plus" data-action="share" data-annotation="vertical-bubble" data-height="60"></div>
+							</li>
+							<li class="facebook">
+								<!-- div class="fb-share-button" data-href="http://www.facebook.com/otonomic/"></div -->
+								<a name="fb_share" type="box_count">Share</a>
+							</li>
+							<li class="tweeter">
+								<a href="https://twitter.com/share" class="twitter-share-button" data-count="vertical">Tweet</a>
+							</li>
+							<li class="linkedin">
+								<script type="IN/Share" data-counter="top"></script>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div><!-- #content ends -->
+	<div id="footer">
+		<div class="row-fluid">
+			<div class="span7 offset5">
+				<div class="pull-right align-right">
+					<img src="assets/img/tablet-dude.png" id="footer-overlap" />
+				</div>
+			</div>
+		</div>
+	</div><!-- #footer ends -->
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=373931652687761&version=v2.0";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+	<script type="text/javascript">
+		(function() {
+		var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+		po.src = 'https://apis.google.com/js/platform.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+		})();
+	</script>
+	<script>
+		!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+	</script>
+	<script src="http://static.ak.fbcdn.net/connect.php/js/FB.Share" type="text/javascript"></script>
+  </body>
+</html>
