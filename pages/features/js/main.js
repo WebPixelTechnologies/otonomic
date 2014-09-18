6ce6602aa8e73802f2ca916c54fba95283ad810f
Denis Driamov <denis@otonomic.com>
2014-09-18 18:32:22 +0300

    Features Page

---
+++ pages/features/js/main.js
@@ -0,0 +1,56 @@
+$(function() {
+
+    // Change nav-Bar appearance on scroll below Jumbotron
+    $(window).on('scroll', function() {
+        var scrollTop = $(this).scrollTop();
+        var toggleFlag = $('.navbar').hasClass('scrolled');
+        //console.log(scrollTop);
+        //console.log(toggleFlag);
+        if ( (scrollTop <= 300) && (toggleFlag) ) {
+            $('.navbar').removeClass('scrolled');
+        }
+        else if((scrollTop > 300) && (!toggleFlag)){
+            $('.navbar').addClass('scrolled');
+        }
+    });
+
+    // Side menu button toggle
+    function openCloseMenu(){
+        //if open then close
+          if ($("#menu-toggle").hasClass('active')) {
+            $(".navbar").animate({paddingRight:"0"}, 300,"swing");
+            $("#sidebar-wrapper").animate({right: "-250", opacity:"0"}, 300,"swing", function() {
+                // Animation complete.
+                $("#menu-toggle").toggleClass("active");
+              });
+          }
+          // else open
+          else{
+            $(".navbar").animate({paddingRight:"250"}, 300,"swing");
+            $("#sidebar-wrapper").animate({right: "0",opacity:"1"}, 300,"swing", function() {
+                // Animation complete.
+                $("#menu-toggle").toggleClass("active");
+              });
+          }
+    }
+
+    function closeMenu(){
+        //if open then close
+        $(".navbar").animate({right: "0",paddingRight:"0",left:"0"}, 300,"swing");
+        $("#sidebar-wrapper").animate({right: "-250", opacity:"0"}, 300,"swing", function() {
+            // Animation complete.
+            $("#menu-toggle").toggleClass("active");
+        });  
+      }
+
+      $("#menu-toggle").click(function(event) {
+          event.preventDefault();
+          openCloseMenu();
+      });
+      $("section").click(function() {
+        //closeSearch('.search-wrapper');
+        if ($("#menu-toggle").hasClass('active')) {
+          closeMenu();
+        }
+    });
+});
