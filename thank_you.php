c2a9b43a0dff24a3223525fb5a1c0b013ee5b57e
Omri Allouche <omri@otonomic.com>
2014-10-25 23:06:27 +0300

    Send emails with all submitted fields

--- thank_you.php
+++ thank_you.php
@@ -2,18 +2,25 @@
 /* send email with lead details */
 $mail_to = 'edik@otonomic.com';
 
-$headers = 'From: leads@otonomic.com' . "\r\n" .
-    'Reply-To: leads@otonomic.com' . "\r\n" .
+$headers = 'From: omri@otonomic.com' . "\r\n" .
+    'Reply-To: omri@otonomic.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
 $ip = $_SERVER['REMOTE_ADDR']?:($_SERVER['HTTP_X_FORWARDED_FOR']?:$_SERVER['HTTP_CLIENT_IP']);
 
 $server = isset($_SERVER['HTTP_HOST'])?$_SERVER['HTTP_HOST']:$_SERVER['SERVER_NAME'];
 $mail_subject = 'New lead generated on '.$server;
+
+foreach($_REQUEST as $key=>$value) {
+    $mail_content = ucfirst(str_replace("_", " ", $key)).": ".$value."\n";
+}
+
+/*
 $mail_content = "Page Name: ".$_REQUEST['page_name'];
 $mail_content .= "\nPage ID: ".$_REQUEST['page_id'];
 $mail_content .= "\nPage Category: ".$_REQUEST['category'];
-$mail_content .= "\nIP: ".$ip;
+*/
+$mail_content .= "IP: ".$ip;
 
 @mail($mail_to, $mail_subject, $mail_content, $headers);
 
