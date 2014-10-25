88545cafd33fd1430058816a1cdc6851a5687c93
Omri Allouche <omri@otonomic.com>
2014-10-25 23:03:08 +0300

    Added LP based on dropbox

--- js/search_filter_lead_gen.js
+++ js/search_filter_lead_gen.js
@@ -4,7 +4,7 @@ var p2s_site_url = 'http://builder.otonomic.com/';
 var p2s_site_creation_base_url = 'http://wp.otonomic.com/migration/';
 
 var ot_wp_site_creation_url = "http://wp.otonomic.com/migration/index.php?theme=parallax";
-var ot_loading_page_url     = "/thank_you.php";
+var ot_loading_page_url     = "/thank_you.html";
 if(typeof (query_tags) == 'undefined') {
     query_tags = {};
 }
