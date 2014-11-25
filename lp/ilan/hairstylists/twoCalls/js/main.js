f389c996d8045eb7b31964165a858b30d9bf3d26
Omri Allouche <omri@otonomic.com>
2014-11-25 18:55:20 +0200

    Changed shared link

--- lp/ilan/hairstylists/twoCalls/js/main.js
+++ lp/ilan/hairstylists/twoCalls/js/main.js
@@ -39,7 +39,7 @@ function shareOnFB()
 {
 	var e = {
 		method: "feed",
-		link: "",
+		link: "http://www.otonomic.com/lp/ilan/hairstylists/twoCalls/",
 		picture: "http://www.otonomic.com/images/hairstyleWebsite-theme-154x113_4x.jpg",
 		name: "Free website for your hair salon - only 45 Hours left!",
 		caption: 'Otonomic.com',
