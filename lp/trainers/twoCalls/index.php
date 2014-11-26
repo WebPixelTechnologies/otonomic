b27b4d6cd6332b996b065129663c84e42e07bc3d
Denis Driamov <sixsatori@gmail.com>
2014-11-26 16:38:22 +0200

    Trainers landing page

---
+++ lp/trainers/twoCalls/index.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Otonomic - Trainers</title>
+
+    <!-- Bootstrap -->
+    <link href="css/styles.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&appId=373931652687761&version=v2.0";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+    <div class="container-fluid">
+      <div class="row">
+          <header class="header">
+            <img class="logo-img" src="images/otonomic-logo.png" alt="otonomic.com">
+            <div class="fb-like pull-right" data-href="https://www.facebook.com/otonomic" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
+          </header>
+          <div class="splash">
+            <div class="splash-title-container">
+              <h1 class="splash-title">Big ass call to action title</h1>
+              <ul class="splash-sub-title">
+                <li>1 Enter your business name</li>
+                <li>2 Get your website</li>
+                <li>3 Edit your website’s content</li>
+                <li>4 Publish your awsome website</li>
+              </ul>
+            </div>
+            <img class="splash-img" src="images/splash-img.jpg">
+            <!-- Search -->
+            <div class="search-container">
+              <form class="form-inline" role="form">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Enter your Facebook page name (or URL)">
+                </div>
+                <button type="submit" class="btn btn-success">Get my Website!</button>
+              </form>
+            </div>
+            <div class="splash-bottom-text">Join <span class="green-text"
+            >123500</span> hair salons all over the world</div>
+          </div>
+      </div>
+    </div>
+    <div class="features">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12 col-md-4">
+            <div class="feature">
+              <img src="images/img-1.svg">
+              <h3>Self-updating from facebook</h3>
+              <p>It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather.</p>
+            </div>
+          </div>
+          <div class="col-xs-12 col-md-4">
+            <div class="feature">
+              <img src="images/img-2.svg">
+              <h3>Web, tablet and mobile ready</h3>
+              <p>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin.</p>
+            </div>
+          </div>
+          <div class="col-xs-12 col-md-4">
+            <div class="feature">
+              <img src="images/img-3.svg">
+              <h3>Web, tablet and mobile ready</h3>
+              <p>It wasn't a dream. His room, a proper human room although a little too small, lay peacefully between its four familiar walls.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="testimonials">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12 col-md-5 col-md-offset-1">
+            <div class="media">
+              <div class="media-left">
+                <img src="images/testi-img-1.jpg" alt="Ashley Millsaps ">
+              </div>
+              <div class="media-body">
+                <h4 class="media-heading">Clare Cosinsky</h4>
+                <div class="media-rating">
+                  <span class="glyphicon glyphicon-star active"></span>
+                  <span class="glyphicon glyphicon-star active"></span>
+                  <span class="glyphicon glyphicon-star active"></span>
+                  <span class="glyphicon glyphicon-star"></span>
+                  <span class="glyphicon glyphicon-star"></span>
+                </div>
+                <p>I've had my website for a little over a month now and my clients absolutely love it!!! They have told me that it's extremely easy to navigate and it looks awesome! I couldn't have had a better website!</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-12 col-md-5">
+            <div class="media">
+              <div class="media-left">
+                <img src="images/testi-img-2.jpg" alt="Jasmine A. Cox">
+              </div>
+              <div class="media-body">
+                <h4 class="media-heading">Stuart Ronson</h4>
+                <div class="media-rating">
+                  <span class="glyphicon glyphicon-star active"></span>
+                  <span class="glyphicon glyphicon-star active"></span>
+                  <span class="glyphicon glyphicon-star active"></span>
+                  <span class="glyphicon glyphicon-star active"></span>
+                  <span class="glyphicon glyphicon-star"></span>
+                </div>
+                <p>I had been wanting a website for my small business for too long. I never found the time or energy to get it done. I'm so grateful, I wish I had done it sooner.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div class="row">
+          <div class="splash2">
+            <div class="splash-title-container">
+              <h1 class="splash-title">Big ass call to action title N0 2</h1>
+            </div>
+            <img class="splash-img" src="images/splash-img-2.jpg">
+            <!-- Search -->
+            <div class="search-container">
+              <form class="form-inline" role="form">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Enter your Facebook page name (or URL)">
+                </div>
+                <button type="submit" class="btn btn-success">Get my Website!</button>
+              </form>
+            </div>
+            <div class="splash-bottom-text">Trusted by more than <span class="green-text"
+            >30000</span> small Businesses in the U.S</div>
+          </div>
+      </div>
+      <div class="row">
+        <footer>
+          <div class="footer-text">
+            <a href="http://otonomic.com/terms" target="_blank">Terms of Use</a> | 
+            <a href="http://otonomic.com/pdfs/Otonomic_Privacy_Policy.pdf" target="_blank">Privacy Policy</a> | 
+            <a href="http://support.otonomic.com/" target="_blank">FAQ</a>   |   
+            <a href="mailto:info@otonomic.com" target="_blank">Contact</a>
+            © 2014
+          </div>
+        </footer>
+      </div>
+    </div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.js"></script>
+    <script type="text/javascript">
+      $(function(){
+        // Services section show "More" btn
+        $('.js-more').click(function(){
+          $(this).parent().find('.caption-content').removeClass('ellipsis');
+          $(this).remove();
+        });
+      });
+    </script>
+  </body>
+</html>
