de6385facc95a5b4aa4c64b2b4edad1d460bf7f8
Denis Driamov <denis@otonomic.com>
2014-12-01 18:34:05 +0200

    Hair styles old  LP into bootstrap responsive

--- lp/ilan/hairstylists/twoCalls/index.php
+++ lp/ilan/hairstylists/twoCalls/index.php
@@ -5,7 +5,8 @@
     <script src="//cdn.optimizely.com/js/326727683.js"></script>
 
     <meta charset="utf-8">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Otonomic - turn your Facebook page into a professional website</title>
     <meta property="og:title" content="Free website for your hair salon - only 45 Hours left!"/>
     <meta property="og:site_name" content="Otonomic"/>
@@ -15,16 +16,8 @@
     <meta property="og:image" content="http://www.otonomic.com/images/hairstyleWebsite-theme-154x113_4x.jpg"/>
 
     <link rel="shortcut icon" href="favicon.ico">
-    <link rel="stylesheet" type="text/css" href="css/style.css?v=0.0.1">
-    <style>
-        body{
-            background: #fcf8f5 url(images/smartphone-bg-pattern.png);
-        }
-        .search_results .close-search{ display: block !important;}
-    </style>
-
+    <link rel="stylesheet" type="text/css" href="css/styles.css?v=0.0.1">
     <link rel="stylesheet" type="text/css" href="css/searchResults.css">
-    <link rel="stylesheet" type="text/css" href="css/media-queries.css?v=0.0.3" />
 
     <!-- Google Analytics Content Experiment code -->
     <script>function utmx_section(){}function utmx(){}(function(){var
@@ -55,88 +48,119 @@
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script type="text/javascript" src="//platform.twitter.com/widgets.js"></script>
     <script type="text/javascript" src="js/main.js?v=1.0"></script>
-    <script src="/js/otonomic-analytics.js"></script>
     <script type="text/javascript" async="" src="http://cdn.luckyorange.com/w.js"></script>
     <script src="/js/otonomic-analytics.js"></script>
 </head>
 
 <body>
-<div class="csszoom" id="smartphone-guy">
-<div class="wrapper">
-    <div id="main">
-        <div class="container2">
-            <header>
-                <div class="logo"><img src="images/logo.png" alt=" " /></div>
-                <div class="top_text">We take your Facebook page and automatically turn it into a Web & Mobile website!</div>
-            </header>
-            <div class="top_wraper2">
-                <h2 class="js-heading-text heading_text">Get your free <br>and beautiful website!</h2>
-                <p class="text2">How? it’s easy!</p>
-                <ul>
-                    <li class="active"><span id="step-1" class="step">1</span> Enter your Facebook business page</li>
-                    <li><span id="step-2" class="step">2</span> Share this promotion with your friends</li>
-                    <li><span id="step-3" class="step">3</span> Sit tight while we get your website ready.</li>
-                </ul>
-                <p class="js-text3 text3" style="opacity: 0;">Spread the word</p>
-                <!-- Search input field -->
-                <div class="p2s_fanpages">
-                  <div class="search-field form-search">
-                    <input id="main_search_box" type="text" 
-                    class="form-control main_search_box LoNotSensitive"
-                    data-attr="center"
-                    onClick="searchBoxClick('#main_search_box');" 
-                    onKeyup="searchBoxKeyUp('#main_search_box','#search_wrapper_main','.close-search');"
-                    placeholder="Type your Facebook page name (or URL)">
-                    <span class="input-group-btn">
-                      <button id="btn_go" class="btn btn_go" data-attr="center" data-target-field="main_search_box" type="button">Get my website</button>
-                    </span>
-                    <span class="close-search" onClick="closeSearch('#search_wrapper_main','center')" style="display: none;">
-                      <img src="/shared/fanpages/images/close.png" width="32" height="32">
-                    </span>
-                  </div>
-                  <div class="tb search-wrapper" id="search_wrapper_main" data-attr="center"></div>
-                </div>
-                <!-- Share Buttons -->
-                <div class="js-social-shares social-shares-container" style="display:none;">
-                    <a href="javascript:void(0)" class="fb-share" onclick="shareOnFB();">
-                        <img src="images/facebook-icon.svg">
-                        Share on facebook
-                    </a>
-                    <a class="tweet" href="https://twitter.com/intent/tweet?url=http://bit.ly/hsalon2&text=Only for 45 Hours! FREE website for your hair salon: @Otonomic turns your Facebook biz page into an amazing website">
-                        <img src="images/twitter-icon.svg">
-                        Share on twitter
-                    </a>             
-                </div>
-            </div>
-            <div style="clear:both"></div>
-            <div class="testimonial">
-                <div class="no_of_website_counting"><span class="counting_no">30,218</span><span>Websites created, and counting...</span></div>
-                <div class="testimonials_wrap">
-                    <div class="testimonail_list">
-                        <div class="user_image"><img src="images/user1.png" alt=" " /></div>
-                        <blockquote><span>I was surprised by how easy it was to create and customize the website.</span></blockquote>
-                    </div>
-                    <div class="testimonail_list">
-                        <div class="user_image"><img src="images/user2.png" alt=" " /></div>
-                        <blockquote><span>After years of putting off building a site, I finally have one that works and looks great.</span></blockquote>
-                    </div>
-                    <div class="testimonail_list">
-                        <div class="user_image"><img src="images/user3.png" alt=" " /></div>
-                        <blockquote><span>Updating my website is a breeze. Love how every post appears on my site immediately.</span></blockquote>
-                    </div>
-                </div>
-            </div>
-            <div class="footer_text"><a href="http://otonomic.com/terms" target="_blank">Terms of Use</a> | <a href="http://otonomic.com/pdfs/Otonomic_Privacy_Policy.pdf" target="_blank">Privacy Policy</a> | <a href="http://support.otonomic.com/" target="_blank">FAQ</a>   |   <a href="mailto:info@otonomic.com" target="_blank">Contact</a>     © 2014
-                <div class="social-channels">
-                    <a class="social-btn facebook-btn" target="_blank" href="https://www.facebook.com/otonomic"><img src="images/facebook-icon.svg"></a>
-                    <a class="social-btn twitter-btn" target="_blank" href="https://twitter.com/otonomic"><img src="images/twitter-icon.svg"></a>
-                    <a class="social-btn linkedin-btn" target="_blank" href="https://www.linkedin.com/company/otonomic"><img src="images/linkedin-icon.svg"></a>
-                    <a class="social-btn googleplus-btn" target="_blank" href="https://plus.google.com/+Otonomic/about"><img src="images/googleplus-icon.svg"></a>
-                </div>
-            </div>
-        </div>
+
+<div class="container-fluid">
+    <div class="row">
+      <header class="header">
+        <img class="logo-img" src="images/otonomic-logo.png" alt="otonomic.com">
+        <div class="header-text">We take your Facebook page and automatically turn it into a Web & Mobile website!</div>
+        <div class="fb-like pull-right hidden-xs hidden-sm" data-href="https://www.facebook.com/otonomic" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
+        <div class="fb-like pull-right hidden-md hidden-lg" data-href="https://www.facebook.com/otonomic" data-layout="button" data-action="like" data-show-faces="false" data-share="false"></div>
+      </header>
     </div>
-</div>
+    <div class="row main-content">
+      <div class="col-xs-12 col-md-11 col-md-offset-1">
+        <!-- Angled wrapper -->
+        <div class="angled-wrapper rr-left">
+          <h1 class="js-heading-text heading-text">Get your free <br>and beautiful website!</h1>
+          <h2 class="text2">How? it’s easy!</h2>
+          <ul>
+              <li class="active"><span id="step-1" class="step">1</span>Enter your Facebook business page</li>
+              <li><span id="step-2" class="step">2</span>Share this promotion with your friends</li>
+              <li><span id="step-3" class="step">3</span>Sit tight while we get your website ready.</li>
+          </ul>
+          <p class="js-text3 text3" style="opacity: 0;">Spread the word</p>
+          <!-- Search input field -->
+          <div class="p2s_fanpages">
+            <div class="form-search form-inline">
+              <input id="main_search_box" type="text" 
+              class="form-control main_search_box LoNotSensitive"
+              data-attr="center"
+              onClick="searchBoxClick('#main_search_box');" 
+              onKeyup="searchBoxKeyUp('#main_search_box','#search_wrapper_main','.close-search');"
+              placeholder="Type your Facebook page name (or URL)">
+              <button id="btn_go" class="btn btn_go" data-attr="center" data-target-field="main_search_box" type="button">Get my website</button>
+              <span class="close-search" onClick="closeSearch('#search_wrapper_main','center')" style="display: none;">
+                <img src="/shared/fanpages/images/close.png" width="32" height="32">
+              </span>
+            </div>
+            <div class="tb search-wrapper" id="search_wrapper_main" data-attr="center"></div>
+          </div>
+          <!-- Share Buttons -->
+          <div class="js-social-shares social-shares-container" style="display:none;">
+              <a href="javascript:void(0)" class="fb-share" onclick="shareOnFB();">
+                  <img src="images/facebook-icon.svg">
+                  Share on facebook
+              </a>
+              <a class="tweet" href="https://twitter.com/intent/tweet?url=http://bit.ly/hsalon2&text=Only for 45 Hours! FREE website for your hair salon: @Otonomic turns your Facebook biz page into an amazing website">
+                  <img src="images/twitter-icon.svg">
+                  Share on twitter
+              </a>             
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row websites-counter">
+      <div class="col-xs-12 col-md-11 col-md-offset-1">
+        <b>30,218</b> Websites created, and counting...
+      </div>
+    </div>
+    <div class="row testimonials">
+      <div class="col-xs-12 col-md-3 col-md-offset-1">
+        <div class="media">
+          <div class="media-left">
+            <img src="images/user1.png" alt="...">
+          </div>
+          <div class="media-body">
+            <p>I was surprised by how easy it was to create and customize the website.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-xs-12 col-md-3">
+        <div class="media">
+          <div class="media-left">
+            <img src="images/user2.png" alt="...">
+          </div>
+          <div class="media-body">
+            <p>After years of putting off building a site, I finally have one that works and looks great.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-xs-12 col-md-3">
+        <div class="media">
+          <div class="media-left">
+            <img src="images/user3.png" alt="...">
+          </div>
+          <div class="media-body">
+            <p>Updating my website is a breeze. Love how every post appears on my site immediately.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <footer>
+        <div class="col-xs-12">
+          <div class="footer-text">
+            <a href="http://otonomic.com/terms" target="_blank">Terms of Use</a> | 
+            <a href="http://otonomic.com/pdfs/Otonomic_Privacy_Policy.pdf" target="_blank">Privacy Policy</a> | 
+            <a href="http://support.otonomic.com/" target="_blank">FAQ</a> |   
+            <a href="mailto:info@otonomic.com" target="_blank">Contact</a> 
+            © 2014
+          </div>
+          <div class="social-channels">
+              <a class="social-btn facebook-btn" target="_blank" href="https://www.facebook.com/otonomic"><img src="images/facebook-icon.svg"></a>
+              <a class="social-btn twitter-btn" target="_blank" href="https://twitter.com/otonomic"><img src="images/twitter-icon.svg"></a>
+              <a class="social-btn linkedin-btn" target="_blank" href="https://www.linkedin.com/company/otonomic"><img src="images/linkedin-icon.svg"></a>
+              <a class="social-btn googleplus-btn" target="_blank" href="https://plus.google.com/+Otonomic/about"><img src="images/googleplus-icon.svg"></a>
+          </div>
+        </div>
+      </footer>
+    </div>
 </div>
     <!-- Search box template -->
     <div style="display: none">
@@ -187,11 +211,9 @@
     <script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.7.1/modernizr.min.js"></script>
     <script type="text/javascript" src="https://d2zxr4ixkv1lwq.cloudfront.net/lp/js/jquery.jsonp-2.4.0.min.js" defer></script>
     <script type="text/javascript" src="/js/search_filterv1.0.4-wp.js?v=1.0.4"></script>
-    <link href="http://d2zxr4ixkv1lwq.cloudfront.net/lp/css/tipsy.css" rel="stylesheet" type="text/css" />
+    <link type="text/css" href="http://d2zxr4ixkv1lwq.cloudfront.net/lp/css/tipsy.css" rel="stylesheet">
     <script type="text/javascript" src="http://d2zxr4ixkv1lwq.cloudfront.net/lp/js/jquery.tipsy.js" defer></script>
     <script type="text/javascript" src="/js/otonomicv1.0.4.js"></script>
-
     <script type="text/javascript" src="/js/functions.js"></script>
-
 </body>
 </html>
