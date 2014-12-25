71658fa273bceafb2ca0ba7f0cffcc36e4d7c272
Denis Driamov <denis@otonomic.com>
2014-12-25 16:03:45 +0200

    Fan page select (Awesome! page ) mobile version

--- shared/facebook_fanpages.php
+++ shared/facebook_fanpages.php
@@ -2,9 +2,11 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="include_files/css/fonts.css">
     <link rel="stylesheet" href="include_files/css/main.css">
-    <title>Page2Site :: Facebook fanpages</title>
+    <title>Otonomic :: Facebook fanpages</title>
 </head>
 <body>
 <nav>
