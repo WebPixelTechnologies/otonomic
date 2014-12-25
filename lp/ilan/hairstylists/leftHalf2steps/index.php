e5066c61e1c0607004f83e25adcdd17ea2f8481a
Denis Driamov <denis@otonomic.com>
2014-12-25 17:09:03 +0200

    On the LPs, on mobile, show “Connect with FB” instead of the search box

--- lp/ilan/hairstylists/leftHalf2steps/index.php
+++ lp/ilan/hairstylists/leftHalf2steps/index.php
@@ -110,7 +110,7 @@ $og_url = $og_variation['url'];
           </ul>
           <p class="js-text3 text3" style="opacity: 0;">Share this promotion with your friends</p>
           <!-- Search input field -->
-          <div class="p2s_fanpages">
+          <div class="p2s_fanpages hidden-xs">
             <div class="form-search form-inline">
               <input id="main_search_box" type="text" 
               class="form-control main_search_box LoNotSensitive"
@@ -125,6 +125,7 @@ $og_url = $og_variation['url'];
             </div>
             <div class="tb search-wrapper" id="search_wrapper_main" data-attr="center"></div>
           </div>
+          <a href="/shared/facebook_login.php" id="fb_connector" class="btn btn-lg btn-facebook-connect visible-xs facebook_connect track_event measure_time" data-ga-category="LandingPage" data-ga-event="Connect with Facebook" data-ga-label="Search explanation box"><img src="images/facebook-icon.svg" alt="Connect with Facebook"> Connect with Facebook</a>
           <!-- Share Buttons -->
           <div class="js-social-shares social-shares-container" style="display:none;">
               <a href="javascript:void(0)" class="fb-share" onclick="shareOnFB();">
